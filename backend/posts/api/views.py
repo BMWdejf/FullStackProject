b880ab3bd1b77cfa4f80bfe9a60bb8faa074e134
BMWdejf <sablatura.david@gmail.com>
2024-03-30 20:28:39 +0100

    Create views.py for posts api

---
+++ backend/posts/api/views.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from ..models import Post
+from .serializers import PostSerializer
+
+class PostViewsSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
