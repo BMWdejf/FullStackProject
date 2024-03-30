3bbf577aba9a71df89baf368d9cefdc40d49c665
BMWdejf <sablatura.david@gmail.com>
2024-03-30 20:25:49 +0100

    Add App rest_fremework, posts and corsheaders to Django

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,7 +41,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
+    "posts",
 ]
+
+# Cors allowed origins
+CORS_ALLOWED_ORIGINS = ['http://localhost:5174']
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -51,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
