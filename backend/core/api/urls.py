c4cb72734b51bef26641f08f8388bcfe42965d03
BMWdejf <sablatura.david@gmail.com>
2024-03-31 18:17:48 +0200

    Add new address_router on urls.py file

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from posts.api.urls import post_router
+from address.api.urls import address_router
 from django.urls import path, include
 
 
 router = DefaultRouter()
 
 router.registry.extend(post_router.registry)
+router.registry.extend(address_router.registry)
 
 urlpatterns = [
     path('', include(router.urls))
